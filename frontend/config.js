d28406bc1f6a1fc89c44ae3fd39cbb85e564db69
Nattasith0 <gunkung.x20@gmail.com>
2026-03-16 15:12:05 +0700

    fix: update service URLs in configuration file

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL: 'https://YOUR-AUTH-DOMAIN',
-  TASK_URL: 'https://YOUR-TASK-DOMAIN',
-  USER_URL: 'https://YOUR-USER-DOMAIN'
+  AUTH_URL: 'https://auth-service-production-d7a0.up.railway.app/',
+  TASK_URL: 'https://task-service-production-cbc1.up.railway.app/',
+  USER_URL: 'https://user-service-production-f14a.up.railway.app/'
 };
